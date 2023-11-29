51e594f9e7aa9406d7d68f2bc3fe0b2ba45ed2c1
Ipaye <Akinkunmi007@gmail.com>
2023-11-29 22:24:51 +0000

    Chore: add middleware for new routes

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ import { withAuth } from 'next-auth/middleware'
 export default withAuth
 
 export const config = {
-  matcher: ['/summarize', '/generate-prompt', '/register']
+  matcher: ['/summarize', '/summary-content', '/generate-prompt', '/register']
 }
