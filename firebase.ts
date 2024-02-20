3ef2e5323e20161d7840a64addccba08064aaa99
Ipaye <Akinkunmi007@gmail.com>
2024-02-20 14:59:19 +0000

    Update Firebase configuration with environment variables

--- firebase.ts
+++ firebase.ts
@@ -5,12 +5,12 @@ import { getFunctions } from 'firebase/functions'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDzdJhPjvp2UQe3W27Pal9Ztc9YqU_4UQM',
-  authDomain: 'edusynthpro.firebaseapp.com',
-  projectId: 'edusynthpro',
-  storageBucket: 'edusynthpro.appspot.com',
-  messagingSenderId: '375546912831',
-  appId: '1:375546912831:web:7f6f41f83e3674d480d88a'
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 }
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
